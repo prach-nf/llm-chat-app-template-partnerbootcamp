9e051c25b7d73acd9dc19b9648cb298a9c82e7a8
prach-nf <prach@nforcesecure.com>
2025-09-24 14:14:34 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -75,12 +75,12 @@ async function handleChatRequest(
       },
       {
         returnRawResponse: true,
-        // Uncomment to use AI Gateway
-        // gateway: {
-        //   id: "YOUR_GATEWAY_ID", // Replace with your AI Gateway ID
-        //   skipCache: false,      // Set to true to bypass cache
-        //   cacheTtl: 3600,        // Cache time-to-live in seconds
-        // },
+         Uncomment to use AI Gateway
+         gateway: {
+           id: "YOUR_GATEWAY_ID", // Replace with your AI Gateway ID
+           skipCache: false,      // Set to true to bypass cache
+           cacheTtl: 3600,        // Cache time-to-live in seconds
+         },
       },
     );
 
